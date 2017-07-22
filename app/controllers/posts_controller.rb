d292fe4b2d7f40268175d0aec036ae438aa8a7fb
Pozetiv <pozetivteam@gmail.com>
2017-07-22 21:09:49 +0300

    dev.2 write something actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,34 @@ class PostsController < ApplicationController
 
   end
 
-  def update
+  def edite
+    @posts = Post.find(params[:id])
   end
 
-  def destroy
+  def delete
+  end
+
+  def create
+    @posts = Post.new(post_params)
+    if @posts.save
+      redirect_to :show
+    else
+      render :new
+    end
+  end
+
+  def update
+    @posts = Post.find(params[:id])
+    if @posts.update_attributes(post_params)
+      redirect_to @posts
+    else
+      redirect_to :edite
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+
   end
 end
