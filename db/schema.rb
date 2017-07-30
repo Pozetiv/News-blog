3f84e086e688e700a0bfab4b0b475d64cf8c8fe0
Pozetiv <pozetivteam@gmail.com>
2017-07-30 22:25:55 +0300

    some update in loyaouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722141726) do
+ActiveRecord::Schema.define(version: 20170730192047) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
