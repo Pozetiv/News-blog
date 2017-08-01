f61d5ddaabcfd9167522367733d68644aa51d660
Pozetiv <pozetivteam@gmail.com>
2017-08-01 21:34:58 +0300

    write validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates :title, :doby, presence: true
+  validates :title, eniqueness:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  validates :username, :email, :password_field, presence: true
+  validates :username, :email, :password, presence: true
+  validates :password, length: {in: 4..16}, massage: "Password must be with length min 4 unite"
+  validates :username, length: {minimum: 3}
+  validates :email, eniqueness: true, massage: "This email are using"
+
 end
