cd1cf6c07d04e00356f23864ffc5b21d05e3075a
Pozetiv <pozetivteam@gmail.com>
2017-08-05 18:01:52 +0300

    update(optimization) controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,38 @@ class UsersController < ApplicationController
  def index
 
  end
+ 
  def new
-   @users = User.new
+   @user = User.new
  end
+
+ def edite
+   @user = User.find(params[:id])
+ end
+
 def create
 @user = User.new(user_params)
   if @user.save
-    redirect_to :show
-    else :new
+    redirect_to @user
+    else render 'new'
   end
 end
 
 def update
-
+@user = User.find(params[:id])
+if @user.update(user_params)
+	redirect_to @user
+else 
+	render 'edite'
 end
 
-def show
-
+def destroy
+@user = User.find(params[:id])
+@user.destroy
+render :new
 end
 
-def delete
-
-end
-
-end
+ private
+  def user_params
+    params.require(:user).permite(:username, :password, :email)
+  end
