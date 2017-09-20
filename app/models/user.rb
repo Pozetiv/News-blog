9956bd95e1d02d24b7158d9784dc6ec25840c56e
Pozetiv <pozetivteam@gmail.com>
2017-09-20 20:59:12 +0300

    1.4.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   #user_name
 before_save {self.email = email.downcase}
   validates :name, presence: true, length:  {in: 3..15}
@@ -19,5 +20,20 @@ before_save {self.email = email.downcase}
     BCrypt::Password.create(string, cost: cost)
   end
 
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
 
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def authenticated?(remember_token)
+      DCrypt::Password.new(remembre_digest).is_password?(remember_token)
+    end
+
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
